aae013de1413e1f9ed34647ee85d7ef92d5bd0dd
Bhargov Jyoti Das <bhargovjyotidas1996@gmail.com>
2022-10-08 15:52:14 +0530

    Quiz App Part 2

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,13 +5,42 @@ void main() {
 }
 
 class MyApp extends StatelessWidget{
+  void answerQuestion (){
+    print("Answer choosen!");
+  }
   @override
   Widget build(BuildContext context){
     return MaterialApp(home:Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
     ),
-    body: Text('This is dummy text'),
+    body: Column(
+      children: [
+        Text("The question"),
+        ElevatedButton(
+          child: Text("Answer 1"),
+          onPressed: answerQuestion,
+        ),
+         ElevatedButton(
+          child: Text("Answer 2"),
+          onPressed: answerQuestion,
+        ),
+         ElevatedButton(
+          child: Text("Answer 3"),
+          onPressed: null,
+        ),
+         ElevatedButton(
+          child: Text("Answer 4"),
+          onPressed: ()=>print("Anon Function"),
+        ),
+         ElevatedButton(
+          child: Text("Answer 5"),
+          onPressed: (){
+            print("Anonn Function");
+            },
+        ),
+      ],
+    )
     ));
   }
 }
