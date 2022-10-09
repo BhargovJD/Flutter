a1610abcec3c1238090819c79d2cbe950857f9c6
Bhargov Jyoti Das <bhargovjyotidas1996@gmail.com>
2022-10-09 13:11:19 +0530

    Quiz App Part 3

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,26 +4,47 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  void answerQuestion (){
-    print("Answer choosen!");
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
   }
+}
+
+class MyAppState extends State<MyApp>{
+  var _qIndex = 0;
+
+    void _answerQuestion (){
+    // print("Answer choosen!");
+    setState(() {
+          _qIndex = _qIndex+1;
+    });
+      print(_qIndex);
+
+  }
+
   @override
   Widget build(BuildContext context){
+    var questions =[
+      "What\'s is your name?",
+      "What\'s is your fav food?",
+    ];
     return MaterialApp(home:Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
     ),
     body: Column(
       children: [
-        Text("The question"),
+        // Text(questions.elementAt(1)),
+        Text(questions[_qIndex]),
         ElevatedButton(
           child: Text("Answer 1"),
-          onPressed: answerQuestion,
+          onPressed: _answerQuestion,
         ),
          ElevatedButton(
           child: Text("Answer 2"),
-          onPressed: answerQuestion,
+          onPressed: _answerQuestion,
         ),
          ElevatedButton(
           child: Text("Answer 3"),
